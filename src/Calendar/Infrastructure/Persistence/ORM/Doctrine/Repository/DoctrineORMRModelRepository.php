0e071ddd49b7e2db401174eff61fc879371e6e05
marianrus <marian.rus@red-to-blue.com>
2018-03-30 14:27:33 +0300

    Created persistence for domain objects.

---
+++ src/Calendar/Infrastructure/Persistence/ORM/Doctrine/Repository/DoctrineORMRModelRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Infrastructure\Persistance\ORM\Doctrine\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class DoctrineORMRModelRepository extends EntityRepository
+{
+
+}
